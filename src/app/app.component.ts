8db51b7cdc75a729e379012c0842939d5f78b54f
choco2105 <danitochococoro2105@gmail.com>
2025-07-11 12:08:03 -0500

    iniital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponentComponent } from './componentFolder/product-component/product-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
