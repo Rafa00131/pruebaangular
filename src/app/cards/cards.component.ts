2b2ceb354a59d2e55c3cb8394595aaddf5daa36e
choco2105 <danitochococoro2105@gmail.com>
2025-07-11 12:51:32 -0500

    interface data

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
-  
   imports: [],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
--- src/app/serviceFolder/product-service.service.ts
+++ src/app/serviceFolder/product-service.service.ts
@@ -11,6 +11,7 @@ export class ProductServiceService {
     return this.httpClient.get(this.ApiURL);
   }
   getProduct() {
+    )
     
   }
 }
